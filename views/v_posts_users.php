095100d9386cec45388d0e2bf8dfd7994b97a95b
CJ Sheets <sheets@g.harvard.edu>
2013-10-23 19:24:18 -0500

    Add controller and views associated with posts

---
+++ views/v_posts_users.php
@@ -0,0 +1,19 @@
+<h1>Follow Users</h1>
+
+<? foreach($users as $user): ?>
+
+	<!-- Print this user's name -->
+	<h2><?=$user['first_name']?> <?=$user['last_name']?></h2>
+	
+	<!-- If there exists a connection with this user, show a unfollow link -->
+	<? if(isset($connections[$user['user_id']])): ?>
+		<a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
+		
+	<!-- Otherwise, show the follow link -->
+	<? else: ?>
+		<a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
+	<? endif; ?>
+	
+	<br><br>
+
+<? endforeach; ?>
