98da449e8040b93ad27c43b8f036d16c01db8ebf
CJ Sheets <sheets@g.harvard.edu>
2013-10-25 13:34:41 -0500

    Add structure to _v_template
    Add google web fonts, sections and divs

--- views/_v_template.php
+++ views/_v_template.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+	<!-- Start of Google Web Fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Lato:400,700|Roboto+Slab:300' rel='stylesheet' 	type='text/css' />
+	<!-- End of Google Web Fonts -->
+
 	<title><?php if(isset($title)) echo $title; ?></title>
 
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	
+	<link rel="stylesheet" type="text/css" href="/css/style.css">	
 					
 	<!-- Controller Specific JS/CSS -->
 	<?php if(isset($client_files_head)) echo $client_files_head; ?>
@@ -12,36 +19,52 @@
 
 <body>
 
-<header>
-	<nav>
-	
+<header class="mainHeader">
+	<div id="headerContent">
+		<img src="/images/logo.png" width="300px" height="138px" align="left" alt="logo">
 		
+			<nav>
+				<!-- Menu for logged in users -->
+				<?php if($user): ?>
+						
+				<a href='/posts'>Home</a>
+				<a href='/posts/users'>Connect</a>
+				<a href='/users/profile'>Profile</a>			
+				<a href='/users/logout'>Sign out</a>
+						
+						
+				<!-- Menu for users who are not logged in -->
+				<?php else: ?>
+						
+				<a href='/'>Home</a>
+				<a href='/users/signup'>Sign up</a>
+				<a href='/users/login'>Sign in</a>
+						
+				<?php endif; ?>
+			</nav>
 			
-			<!-- Menu for logged in users -->
-			<?php if($user): ?>
-			
-			<a href='/posts'>Home</a>
-			<a href='/posts/users'>Follow</a>
-			<a href='/users/profile'>Profile</a>			
-			<a href='/users/logout'>Sign out</a>
-			
-			<!-- Menu for users who are not logged in -->
-			<?php else: ?>
-			
-			<a href='/'>Home</a>
-			<a href='/users/signup'>Sign up</a>
-			<a href='/users/login'>Sign in</a>
-			
-			<?php endif; ?>
-		</ul>
-	</nav>
+	</div> <!-- End of headerContent -->
 </header>
 
-
+<div id="wrapper">
 	
+	<section class="content">
+		<?php if(isset($content)) echo $content; ?>
+	
+		<?php if(isset($client_files_body)) echo $client_files_body; ?>
+	</section>
 
-	<?php if(isset($content)) echo $content; ?>
+<aside>
+</aside>
 
-	<?php if(isset($client_files_body)) echo $client_files_body; ?>
+</div> <!-- End of wrapper -->
+
 </body>
+
+
+<!--
+<footer>
+		<p>© 2013 YapperBox</p>
+</footer>
+-->
 </html>
