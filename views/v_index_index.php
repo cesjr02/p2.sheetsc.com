048d8310a57bc4ef074e4c4d83fbcca1883ee583
CJ Sheets <sheets@g.harvard.edu>
2013-10-26 00:17:58 -0500

    Update content, include v_users_login.php

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,4 +1,14 @@
-<h1>Welcome to <?=APP_NAME?><?php if($user) echo ', '.$user->first_name; ?></h1>
+<section class="content">
+	<h1>Welcome to <?=APP_NAME?><?php if($user) echo ', '.$user->first_name; ?></h1>
 
-<p>Yapperbox is a community where users can easily connect with others and share ideas. You can even follow other users so you always stay in the loop. Start yapping today!
-</p>
+	<p>
+	Yapperbox is a community where users can easily connect with others and share ideas. Signup 		and start connecting so you always stay in the loop. Start yapping today!
+	</p>
+
+	<img src="/images/main-info-graphic.png" width="540px" height="462px" align="left" class="main-info-graphic" alt="info graphic">
+		
+</section>
+
+<aside>
+	<?php include('v_users_login.php'); ?>
+</aside>
