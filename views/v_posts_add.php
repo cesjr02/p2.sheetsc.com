5ef0eb6e997af51a7624d761bbb89672adaffa61
CJ Sheets <sheets@g.harvard.edu>
2013-10-31 11:19:12 -0500

    add maxlength of 255 to text area

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,7 +1,7 @@
 <form method='POST' action='/posts/p_add'>
 
 	<h2><label for='content'>Yap about something.</label></h2>
-	<textarea name='content' id='content' class='textarea' required></textarea>
+	<textarea maxlength='255' name='content' id='content' class='textarea' required></textarea>
 	<br>
 	<input type='submit' class='button 'value='Yap'>
 	
