b1b8ba1a6e1e54fcf15a601e9a7980990adb6d05
CJ Sheets <sheets@g.harvard.edu>
2013-10-31 11:16:14 -0500

    add +1 features and modify content

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -2,9 +2,22 @@
 	<h1>Welcome!</h1>
 
 	<p>
-	Welcome to Yapperbox. Yapperbox is a community where users can easily connect with others and share ideas. Register and start connecting so you always stay in the loop. Start yapping today!
+	Welcome to Yapperbox. Yapperbox is a community where users can easily connect with others and share ideas. Signup and start making connections so you always stay in the loop. Start yapping today!
 	</p>
-
+	
+	<div>
+		<h3 class="features" >+1 Features:</h3>
+		<ul>
+		 	<li>
+			 	<span>Email confirmation upon user signup</span>
+		 	</li>
+		 	
+		 	<li>
+			 	<span>Upload avatar to profile page</span>
+		 	</li>	
+		</ul>
+	</div>
+	
 	<img src="/images/main-info-graphic.png" width="540px" height="462px" align="left" class="main-info-graphic" alt="info graphic">
 		
 </section>
