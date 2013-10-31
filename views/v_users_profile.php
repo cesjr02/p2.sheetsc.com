a8ad380b4fe52b4cc487fc31f181ca4052c7d5ae
CJ Sheets <sheets@g.harvard.edu>
2013-10-31 11:22:54 -0500

    add functionality to change profile picitre
    if $user->image placeholder.jpg, display placeholder. If user uploads
    image, display that image.

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,14 +1,24 @@
 <h2>About Me</h2>
-<h3>This is the profile of <?=$user->first_name?> <?=$user->last_name?>.</h3>
 
-<p><b>Name:</b> <?=$user->first_name?> <?=$user->last_name?></p>       
-        
-<form method='POST' enctype="multipart/form-data" action='/users/p_upload/'>
+<h4>Name: <span><?=$user->first_name?> <?=$user->last_name?></span></h4>
+<h4>Join Date: <span><?= date('F j, Y', $user->created) ?></span></h4>    
+     
+<?php if ($user->image  == 'placeholder.jpg'): ?>
+<?php endif; ?>
+
+<form method='POST' enctype="multipart/form-data" action='/users/p_profile/'>
 	
-	<img src="/uploads/avatars/placeholder.jpg"><br>
-	<label for="file">Upload Photo:</label><br>
+	<img class="avatar" src="/uploads/avatars/<?=$user->image ?>" alt="<?=$user->first_name . ' ' . $user->last_name ?>"><br />
+	<h3 class="update" >Update profile picture:</h3>
 	<input type="file" name="file" id="file"><br>
-	<input type="submit" name="submit" user="Submit">
+	
+	<?php if(isset($error)): ?>
+		<div class="error">
+			Upload failed.<br /> 
+			Upload must be an image file; jpg, gif, or png.
+		</div>
+	<?php endif; ?>
+	
+	<input type="submit" name="submit" value="Update" class="button" id="buttonUpdate" user="Submit">
+	
 <form>
-
-
