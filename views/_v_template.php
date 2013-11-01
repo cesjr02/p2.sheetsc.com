c78473bf901093a9a9c81947669075e1e8c7cb93
CJ Sheets <sheets@g.harvard.edu>
2013-11-01 17:37:58 -0500

    force bookmark icon to refresh

--- views/_v_template.php
+++ views/_v_template.php
@@ -4,7 +4,7 @@
 <head>
 	<!-- Bookmark Icon -->
 	<link rel="icon" type="image/png" href="images/bookmark-icon/myicon.png" />
-	
+	<link rel="shortcut icon" href="http://www.p2.sheetsc.com/images/bookmark-icon/myicon.png?v=2" />
 	
 	<!-- Apple Icon -->      
 	<link rel="apple-touch-icon-precomposed" href="images/bookmark-icon/apple-touch-iphone.png" />
