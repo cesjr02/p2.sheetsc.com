ed21a8dd079f1e6c8f5074ca04ef0d705959723e
CJ Sheets <sheets@g.harvard.edu>
2013-11-01 13:29:10 -0500

    if user is logged on, says hello to user's name. Else, give general welcome

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,6 +1,12 @@
 <section class='contentMain'>
-	<h1>Welcome!</h1>
-
+	<!-- If User is logged in -->
+	<?php if($user): ?>
+    <h1>Hello,  <?php if($user): ?><?php echo $user->first_name; ?><?php endif; ?>.</h1>
+	<!-- If User is NOT logged in -->
+	<?php else: ?>
+    <h1>Welcome!</h1>
+    <?php endif; ?> 
+    
 	<p>
 	Welcome to Yapperbox. Yapperbox is a community where users can easily connect with others and share ideas. Signup and start making connections so you always stay in the loop. Start yapping today!
 	</p>
