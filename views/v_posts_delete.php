3806b1b385de4754221dbeb8f480322e6f718ef3
CJ Sheets <sheets@g.harvard.edu>
2013-11-02 00:23:38 -0500

    add post delete confirmation view

---
+++ views/v_posts_delete.php
@@ -0,0 +1,3 @@
+<h2>Are you sure you want to delete this post?</h2>
+
+<h3><a href='/posts/p_delete/<?=$post['post_id']?>'><input type='submit' value='Delete' class='button' id='buttonDelete' ></a><h3>
