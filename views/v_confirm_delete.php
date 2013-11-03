a58d5a993eecfef155d194763ef63f1041bc6c15
CJ Sheets <sheets@g.harvard.edu>
2013-11-03 15:07:42 -0600

    add section to confirm delete

--- views/v_confirm_delete.php
+++ views/v_confirm_delete.php
@@ -1,7 +1,11 @@
-<h2>Confirm Delete</h2>
+<section class='content'>
 
-<p>Are you sure you want to delete this post? If not, click <a href="/posts">here </a>to return.</p>
+	<h2>Confirm Delete</h2>
+	
+	<p>Are you sure you want to delete this post? If not, click <a href="/posts">here </a>to return.</p>
+	
+	<a href='/posts/delete/<?=$post['post_id']?>'><button type="button" class='button' id='confirmDelete'>Delete</button></a>
 
-<a href='/posts/delete/<?=$post['post_id']?>'><button type="button" class='button' id='confirmDelete'>Delete</button></a>
+</section>section
 
 
