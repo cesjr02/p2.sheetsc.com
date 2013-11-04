f349c49b8696c0158b10bb2b8659d8bf9f9dfce1
CJ Sheets <sheets@g.harvard.edu>
2013-11-04 10:53:27 -0600

    clean up invisible characters

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -18,10 +18,10 @@ class users_controller extends base_controller {
 
 		// setup view
 		$this->template->content = View::instance('v_users_signup');
-		
+
 		// set page title
 		$this->template->title = "Sign up";
-		
+
 		// render view
 		echo $this->template;
 
@@ -35,30 +35,30 @@ class users_controller extends base_controller {
 	
 		// setup view
 		$this->template->content = View::instance('v_users_signup');
-				
+
 		// initial error to false
 		$error = false;
-				
+
 		// initiate error
 		$this->template->content->error = '<br>';
-				
+
 		// if we have no post data just display the View with signup form
 		if(!$_POST) {
 			echo $this->template;
 			return;
 		}
-		
+
 		// otherwise...
 		// loop through the POST data
 		foreach($_POST as $field_name => $value) {
-						
+
 			// if a field was blank, add a message to the error View variable
 			if(trim($value) == "") {
 				$error = true;
 				$this->template->content->error = 'All fields are required.';
 			}
 		} 
-				
+
 		// check whether this user's email already exists (sanitize input first)
 		$_POST = DB::instance(DB_NAME)->sanitize($_POST);
 		$exists = DB::instance(DB_NAME)->select_field("SELECT email FROM users WHERE email = '" . $_POST['email'] . "'");
@@ -68,41 +68,41 @@ class users_controller extends base_controller {
 			$this->template->content->error = 'This email address has already been registered, please try again.';
 			echo $this->template;
 		}
-			
+
 		// check if password is typed correctly
 		else if ($_POST['password'] != $_POST['retype']) {
 			$error = true;
 			$this->template->content->error = 'Password fields don&apos;t match.';
 			echo $this->template;
 		}
-				
+
 		// if no previous errors, add to database
 		else if(!$error) {
 		
 			// add XSS and html tag filtering
 			$firstname = $_POST['first_name'];
 			$firstname = strip_tags(htmlentities(stripslashes(nl2br($firstname)),ENT_NOQUOTES,"Utf-8"));
-										
+
 			// add XSS and html tag filtering
 			$lastname = $_POST['last_name'];
 			$lastname = strip_tags(htmlentities(stripslashes(nl2br($lastname)),ENT_NOQUOTES,"Utf-8"));
-		
+
 			// unset the 'retype' field (not needed in db)
 			unset($_POST['retype']);
-		
+
 			// more data we want stored with the user
 			$_POST['created']  = Time::now();
 			$_POST['modified'] = Time::now();
-				
+
 			// encrypt the password
 			$_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
-				
+
 			// create an encrypted token via their email address and a random string
 			$_POST['token'] = sha1(TOKEN_SALT.$_POST['email'].Utils::generate_random_string());
-				
+
 			// insert this user into the database
 			$user_id = DB::instance(DB_NAME)->insert('users', $_POST);
-				
+
 			// all users follow their own posts by default
 			$data = Array(
 				"created" => Time::now(),
@@ -112,17 +112,17 @@ class users_controller extends base_controller {
 
 			// do the insert
 			DB::instance(DB_NAME)->insert('users_users', $data);
-			
+
 			// log user in using the token we generated
 			setcookie("token", $_POST['token'], strtotime('+1 year'), '/');
-			
+
 			// send an email a welcome message to the new user
 			// build a multi-dimension array of recipients of this email
 			$to[]    = Array("name" => $_POST['first_name'], "email" => $_POST['email']);
 			$from    = Array("name" => APP_NAME, "email" => APP_EMAIL);
 			$subject = "Welcome to YapperBox";
 			$body = View::instance('e_users_welcome');
-				
+
 			// Send email
 			Email::send($to, $from, $subject, $body, true, '');
 
@@ -144,81 +144,81 @@ class users_controller extends base_controller {
 		
 		// setup view
 		$this->template->content = View::instance('v_users_login');
-		
+
 		// setup page title
 		$this->template->title   = "Login";
-		
+
 		// pass data to the view
 		$this->template->content->error = $error;
-		
+
 		// render view
 		echo $this->template;
    
 	} 
-	
+
 /*-------------------------------------------------------------------------------------------------
 	process login form
 -------------------------------------------------------------------------------------------------*/
-	
+
 	public function p_login() {
-		
+
 		// sanitize the user entered data to prevent SQL Injection Attacks
 		$_POST = DB::instance(DB_NAME)->sanitize($_POST);
-		
+
 		// hash submitted password so we can compare it against the one in the DB
 		$_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
-		
+
 		// search the DB for this email and password
 		// retrieve the token if it's available
 		$q = "SELECT token
 			FROM users
 			WHERE email = '".$_POST['email']."'
 			AND password = '".$_POST['password']."'";
-			
+
 		$token = DB::instance(DB_NAME)->select_field($q);
-		
+
 		// looks for matching token in DB
 		if(!$token) {
-			
+
 			// token not found. Login failed, sends user back to login
 			Router::redirect("/users/login/error");
-		
+
 		}
 		else {
-			
+
 			setcookie("token", $token, strtotime('+1 year'), '/');
-			
+
 			// token found, login successful
 			Router::redirect("/users/profile/");
 			// echo "You are logged in!";
 		}
 		
 	} 
-	
+
 /*-------------------------------------------------------------------------------------------------
 	logout, redirects to "/"
 -------------------------------------------------------------------------------------------------*/
 
 	public function logout() {
-		
+
 		// generate and save a new token for next login
 		$new_token = sha1(TOKEN_SALT.$this->user->email.Utils::generate_random_string());
-		
+
 		// create the data array we'll use with the update method
 		// in this care, we're only updated one field, so our array only has one entry
 		$data = Array("token" => $new_token);
-		
+
 		// do the update
 		DB::instance(DB_NAME)->update("users", $data, "WHERE token = '".$this->user->token."'");
-		
+
 		// delete their token cookie by setting it to a date in the past -  effectively logging them out
 		setcookie("token", "", strtotime('-1 year'), '/');
-		
+
 		// send them back to the main index
 		Router::redirect("/");
-		
+
 	} 
-	
+
 /*-------------------------------------------------------------------------------------------------
 
 -------------------------------------------------------------------------------------------------*/
@@ -229,19 +229,19 @@ class users_controller extends base_controller {
 		if(!$this->user) {
 			Router::redirect('/users/login');
 		}
-		
+
 		// if they weren't redirected away, continue:
-		
+
 		// setup view
 		$this->template->content = View::instance('v_users_profile');
 		$this->template->title = $this->user->first_name .' ' . $this->user->last_name. " | Profile";
-		
+
 		// pass errors, if any
 		$this->template->content->error = $error;
-		
+
 		// render view
 		echo $this->template;
-			 
+
 	} 
 	
 /*-------------------------------------------------------------------------------------------------
@@ -291,12 +291,12 @@ class users_controller extends base_controller {
 		$this->template->title = "Delete Account";
 		$this->template->content = View::instance('v_users_unsubscribe');
 		$this->template->content->error = $error;
-		
+
 		// render template
 		echo $this->template;
-	
+
 	}
-	
+
 /*-------------------------------------------------------------------------------------------------
 	process unsubscribe
 -------------------------------------------------------------------------------------------------*/
@@ -307,7 +307,7 @@ class users_controller extends base_controller {
 		if ($_POST['password'] != $_POST['conf_password']) {
 			$error = 'InvalidPassword';
 		}
-		
+
 		else {
 			// sanitize the user entered data to prevent SQL Injection Attacks
 			$_POST = DB::instance(DB_NAME)->sanitize($_POST);
