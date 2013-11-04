729cc362ab6ed11bdd5d80c3d016bca32808eb76
CJ Sheets <sheets@g.harvard.edu>
2013-11-03 18:44:35 -0600

    rename view for delete post

--- views/v_post_delete.php
+++ views/v_post_delete.php
@@ -4,7 +4,7 @@
 	
 	<p>Are you sure you want to delete this post? If not, click <a href="/posts">here </a>to return.</p>
 	
-	<a href='/posts/delete/<?=$post['post_id']?>'><button type="button" class='button' id='confirmDelete'>Delete</button></a>
+	<a href='/posts/p_delete/<?=$post_id?>'><button type="button" class='button' id='confirmDelete'>Delete</button></a>
 
 </section>
 
