378840eae7565dfd47238418eed1b592e838ac48
CJ Sheets <sheets@g.harvard.edu>
2013-11-05 11:10:32 -0600

    add section div, and missing form end tag

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,30 +1,36 @@
-<h2>My Profile</h2>
+<section class='content'>
 
-<!-- displays user's name and date joined -->
-<h4>Name: <span><?=$user->first_name?> <?=$user->last_name?></span></h4>
-<h4>Join Date: <span><?= date('F j, Y', $user->created) ?></span></h4>    
-
-<!-- displays placeholder image for new users -->     
-<?php if ($user->image  == 'placeholder.jpg'): ?>
-<?php endif; ?>
-
-<form method='POST' enctype="multipart/form-data" action='/users/p_profile/'>
-
-	<!-- displays user image if uploaded -->
-	<img class="avatar" src="/uploads/avatars/<?=$user->image ?>" alt="<?=$user->first_name . ' ' . $user->last_name ?>"><br />
-	<h4 id="update" >Update profile picture:</h4>
-	<input type="file" name="file" id="file"><br>
+	<h2>My Profile</h2>
 	
-	<!-- error if wrong file type is uploaded -->
-	<?php if(isset($error)): ?>
-		<div class="error">
-			Upload failed.<br /> 
-			Upload must be an image file; jpg, gif, or png.
-		</div>
+	<!-- displays user's name and date joined -->
+	<h4>Name: <span><?=$user->first_name?> <?=$user->last_name?></span></h4>
+	<h4>Join Date: <span><?= date('F j, Y', $user->created) ?></span></h4>    
+	
+	<!-- displays placeholder image for new users -->     
+	<?php if ($user->image  == 'placeholder.jpg'): ?>
 	<?php endif; ?>
 	
-	<input type="submit" name="submit" value="Update" class="button" id="buttonUpdate" user="Submit">	
-<form><br>
-<hr>
-<h4>Delete Account</h4>
-Click<a href="/users/unsubscribe/"> here </a>if you wish to delete your account.
+	<form method='POST' enctype="multipart/form-data" action='/users/p_profile/'>
+	
+		<!-- displays user image if uploaded -->
+		<img class="avatar" src="/uploads/avatars/<?=$user->image ?>" alt="<?=$user->first_name . ' ' . $user->last_name ?>"><br />
+		<h4 id="update" >Update profile picture:</h4>
+		<input type="file" name="file" id="file"><br>
+		
+		<!-- error if wrong file type is uploaded -->
+		<?php if(isset($error)): ?>
+			<div class="error">
+				Upload failed.<br /> 
+				Upload must be an image file; jpg, gif, or png.
+			</div>
+		<?php endif; ?>
+		
+		<input type="submit" name="submit" value="Update" class="button" id="buttonUpdate" user="Submit">
+			
+	</form><br>
+	<hr>
+	<h4>Delete Account</h4>
+	Click<a href="/users/unsubscribe/"> here </a>if you wish to delete your account.
+
+</section>
+
